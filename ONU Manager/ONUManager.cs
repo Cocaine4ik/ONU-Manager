4125603baa4df653d64da760459778c78593fdb1
kokotkin.alexander <kokotkin.alexander@gmail.com>
2018-08-27 22:52:08 +0300

    min telnet library

--- ONU Manager/ONUManager.cs
+++ ONU Manager/ONUManager.cs
@@ -10,8 +10,35 @@ namespace ONU_Manager
     {
         static void Main(string[] args)
         {
-            TelnetClient a = new TelnetClient();
-            a.Connect("10.10.110.115", 23);
+            //create a new telnet connection to hostname "gobelijn" on port "23"
+            TelnetConnection tc = new TelnetConnection("10.10.", 23);
+
+            //login with user "root",password "rootpassword", using a timeout of 100ms, and show server output
+            string s = tc.Login("root", "rootpassword", 100);
+            Console.Write(s);
+
+            // server output should end with "$" or ">", otherwise the connection failed
+            string prompt = s.TrimEnd();
+            prompt = s.Substring(prompt.Length - 1, 1);
+            if (prompt != "$" && prompt != ">")
+                throw new Exception("Connection failed");
+
+            prompt = "";
+
+            // while connected
+            while (tc.IsConnected && prompt.Trim() != "exit")
+            {
+                // display server output
+                Console.Write(tc.Read());
+
+                // send client input to server
+                prompt = Console.ReadLine();
+                tc.WriteLine(prompt);
+
+                // display server output
+                Console.Write(tc.Read());
+            }
+            Console.WriteLine("***DISCONNECTED");
+            Console.ReadLine();
         }
-    }
 }
