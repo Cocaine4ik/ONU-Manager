4d6d31f3fe96aeb163918cc2c561068097c50e8d
Cocaine4ik <kokotkin.alexander@gmail.com>
2018-09-18 16:59:06 +0300

    Delete Rx 1-st check. Add AOE Sollution

--- ONU Manager/ONUManager.cs
+++ ONU Manager/ONUManager.cs
@@ -10,84 +10,83 @@ namespace ONU_Manager
     class ONUManager
     {
         static void Main(string[] args)
-        {  
-            string input = ""; // client input
-            string output = ""; // server output
-            string sn = "";
+        {   string login;
+            string input; // client input
+            string output; // server output
+            string sn;
             int ponNumber = 0; // pon number
             int onuNumber = 0; // onu number
-            int vlan = 1000;
+            int vlan = 1000; // default vlan
 
-            //create a new telnet connection to hostname "10.10.110.115" on port "23"
+            // create a new telnet connection to hostname "10.10.110.115" on port "23"
             TelnetConnection tc = new TelnetConnection("10.10.110.115", 23);
 
-            //login with user "admin", password "admin", using a timeout of 100ms, and show server output
-            string s = tc.Login("admin", "admin", 100);
-            Console.Write(s);
-
-            // read OLT answer after log in
+            // login with user "admin", password "admin", using a timeout of 100ms, 
+            // and show server output
+            login = tc.Login("admin", "admin", 100);
+            Console.Write(login);
             Console.Write(tc.Read());
 
-            // show unconfig onu
-            input = "show gpon onu uncfg";
-            tc.WriteLine(input);
+            // show onu with out configure
+            tc.WriteLine("show gpon onu uncfg");
 
+            // make condition when we haven't got onu to configure
             output = tc.Read();
-            if(output.Contains("No related information to show"))
-            Console.WriteLine("There are nothing to configure now!");
-            else Console.Write(output);
+            if(output.Contains("No related information to show")) {
 
-            // Must work correclt only if we have one or more onu to register
-            
-            // (157, 3) - pon number area
-            string comparePon = output.Substring(157, 3);
-            // (169, 12) - serial number area
-            sn = output.Substring(169, 12);
-
-
-
-            // check substring comparePon 
-            // Console.WriteLine(comparePon);
-            // Console.WriteLine(sn);
-            if (comparePon.IndexOf(":") == 1)
-                ponNumber = Convert.ToInt32(comparePon.Substring(0,1));
-                    else ponNumber = Convert.ToInt32(comparePon.Substring(0,2));
-            
-            // Console.WriteLine(ponNumber);
-            tc.WriteLine("terminal length 0");
-            tc.WriteLine("show gpon onu state gpon-olt_1/2/" + ponNumber);
-            output = tc.Read();
-            Console.Write(output);
-
-            // search for "ONU Number:" substring
-            int slotIndex = output.IndexOf("ONU Number:");
-            Console.WriteLine(slotIndex);
-            String checkOnuNumber = output.Substring(slotIndex + 12, 3);
-            Console.WriteLine(checkOnuNumber);
-            if(checkOnuNumber.IndexOf("/") == -1) {
-                onuNumber =  Convert.ToInt32(output.Substring(slotIndex + 15, 3));
-            Console.WriteLine(onuNumber);
+                Console.WriteLine("There are nothing to configure now!");
             }
-             else if (checkOnuNumber.IndexOf("/") == 2) {
-                onuNumber =  Convert.ToInt32(output.Substring(slotIndex + 15, 2));
-                Console.WriteLine(onuNumber);
-             }
-            else {
-                onuNumber =  Convert.ToInt32(output.Substring(slotIndex + 15, 1));
-                Console.WriteLine(onuNumber);
-            }
-            onuNumber += 1;
+            else  {
+                Console.Write(output);
+            
+                // (157, 3) - pon number area
+                string comparePon = output.Substring(157, 3);
+                // (169, 12) - serial number area
+                sn = output.Substring(169, 12);
 
-            // write default vlan
-            vlan = 1000 + ponNumber;
+                // checking substring comparePon 
+                // some troubles here whene we register to 1 numeral pon!!!
+                if (comparePon.IndexOf(":") == 1)
+                    ponNumber = Convert.ToInt32(comparePon.Substring(0,1));
+                        else ponNumber = Convert.ToInt32(comparePon.Substring(0,2));
+            
+                // remove the restriction on the output of text to the size of the window
+                tc.WriteLine("terminal length 0");
+                // show all onu in "ponNumber" pon
+                tc.WriteLine("show gpon onu state gpon-olt_1/2/" + ponNumber);
+                output = tc.Read();
+                Console.Write(output);
 
-            tc.WriteLine("configure terminal");
-            Console. Write(tc.Read());
+                // search for "ONU Number:" substring
+                int slotIndex = output.IndexOf("ONU Number:");
+
+                String checkOnuNumber = output.Substring(slotIndex + 12, 3);
+                if(checkOnuNumber.IndexOf("/") == -1) {
+                    onuNumber =  Convert.ToInt32(output.Substring(slotIndex + 15, 3));
+                Console.WriteLine(onuNumber);
+                }
+                else if (checkOnuNumber.IndexOf("/") == 2) {
+                    onuNumber =  Convert.ToInt32(output.Substring(slotIndex + 15, 2));
+                Console.WriteLine(onuNumber);
+                }
+                else {
+                    onuNumber =  Convert.ToInt32(output.Substring(slotIndex + 15, 1));
+                Console.WriteLine(onuNumber);
+                }
+
+                // make value next to last onu number value
+                onuNumber += 1;
+
+                // change vlan to standart
+                vlan = 1000 + ponNumber;
+            
+                tc.WriteLine("configure terminal");
+                Console. Write(tc.Read());
 
             tc.WriteLine("interface gpon-olt_1/2/" + ponNumber);
             tc.WriteLine("onu " + onuNumber + " type universal sn " + sn);
-            Console. Write(tc.Read());
-            
+            Console. Write(tc.Read());          
+
             tc.WriteLine("onu " + onuNumber + " profile line 500m");
             Console. Write(tc.Read());
 
@@ -123,9 +122,7 @@ namespace ONU_Manager
             tc.WriteLine("show pon power onu-rx gpon-onu_1/2/" + ponNumber + ":" + onuNumber);
             Console. Write(tc.Read());
             
-            tc.WriteLine("show pon power onu-rx gpon-onu_1/2/" + ponNumber + ":" + onuNumber);
-            Console. Write(tc.Read());
-
+            }
             // exit from OLT console interface
            // Console.WriteLine("Press any key to exit.");
             //tc.WriteLine("exit");
---
+++ ONU-Manager-AOE/ONUManagerAOE.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ONU_Manager_AOE {
+    class ONUManagerAOE {
+        static void Main(string[] args) {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            process.StartInfo = new System.Diagnostics.ProcessStartInfo("D:\\C# Projects\\ONU-Manager\\ONU-Manager\\bin\\Debug\\ONU Manager.exe");
+            process.Start();
+        }
+    }
+}
